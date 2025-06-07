0228747d3efd84415b3cf31364a3940f0d988ab7
Maxouwe <100154435+Maxouwe@users.noreply.github.com>
2025-06-07 17:02:28 +0200

    Comments en uitleg

--- features.py
+++ features.py
@@ -2,3 +2,17 @@ def check_words(row):
     words_column1 = set(row['product_title'].lower().split())
     words_column2 = set(row['search_term'].lower().split())
     return int(words_column2.issubset(words_column1))
+
+def getPredictedRelevance(row):
+    probs = [row['p1'], row['p2'], row['p3']]
+    max = 0
+    classification = -1
+    for i in [0, 1, 2]:
+        if probs[i] > max:
+            max = probs[i]
+            classification = i + 1
+    return classification
+
+
+    
+
--- main.py
+++ main.py
@@ -3,9 +3,15 @@ import pandas as pd
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import r2_score
+from sklearn.metrics import r2_score, confusion_matrix, ConfusionMatrixDisplay
 from collections import Counter
 import features
+from statsmodels.miscmodels.ordinal_model import OrderedModel
+
+
+#There are two sections here 
+#First section shows how to do linear regression
+#Second section shows how to do ordinal logistic regression
 
 # Specify the path to your CSV file
 csv_path = "resources/query_product.csv"
@@ -40,7 +46,7 @@ model = sm.OLS(y, X)
 # If not then we should adjust/make a new feature
 # Here you can see all_words_in_title is a significant feature
 results = model.fit()
-print(results.summary())
+# print(results.summary())
 
 #results now contain the model for the relevance score based on your features
 #now you just have to inject the test data into the model
@@ -91,4 +97,89 @@ plt.legend()
 
 #all the previous plt.f() calls modify some plot object
 #call plt.show() to actually show the plot
+plt.show()
+
+##########################################################################################################
+#so that concludes the linear regression part now there will be an example for ordinal logistic regression
+#you have to close the last plot so the program can to continue i think
+##########################################################################################################
+
+#create logistic train and test data
+#.where() is basically like WHERE in SQL
+#here we want to only get the tuples where relevance is an integer
+#the weird thing about .where() is that it still returns all tuples
+#but for all tuples that do not meet the where-requirement the column values are replaced with NaN
+#use .dropna() to remove these NaN tuples
+logit_train_data = train_data.where(train_data.relevance == np.floor(train_data.relevance)).dropna()
+logit_test_data = test_data.where(test_data.relevance == np.floor(test_data.relevance)).dropna()
+
+#the all_words_in_title feature was already applied to the data in the linear regression section
+#ordinal logistic regression does not use an intercept so we dont add a column of 1's to X like we did in linear regression
+X = logit_train_data['all_words_in_title']
+y = logit_train_data['relevance']
+
+
+#create ordinal logistic model object
+logit_model = OrderedModel(y, X, distr='logit')
+
+#calculate coefficients and thresholds
+#het model berekent een waarde bepaalde waard x 
+#en binnen in het model wordt deze waarde x gebruikt om de input the classificeren
+#Dus deze waarde x zie je als gebruiker niet
+#in de summary zie je 3 coefficienten een voor de allwordsintitle feature
+#en twee thresholds
+#als de onzichtbare waarde x < threshold 1.0/2.0 dan is de output relevance = 1
+#als x tussen de twee thresholds ligt is output relevance = 2
+#is x groter dan threshold 2.0/3.0 dan is de output relevance = 3
+#ook hier kun je aflezen met P>|Z| hoe significant de coefficienten zijn (net zoals bij linear regression)
+#het blijkt dat alle coefficienten hier significant zijn
+#dus ook voor ordinal logistic regression is all_words_in_title een goede feature
+logit_results = logit_model.fit()
+# print(logit_results.summary())
+
+#now we are going to test the model on the test data
+X_test = logit_test_data['all_words_in_title']
+y_test = logit_test_data['relevance']
+
+#see how the model classifies the test_data tuples
+#the linear regression predict() function can take the data directly
+#but for the logistic regression predict() you for some reason first need to cast 
+#the data to an numPy.Array()
+#the output of OrderedModel.predict() 
+#for each tuple x we get output [p1, p2, p3]
+#where p1 is the chance that x has relevance score 1, p2 to relevance score 2, p3 to relevance score 3
+y_pred = logit_results.predict(np.array(X_test))
+
+#so to extract the predicted class we have to choose the highest probability from the array for each tuple
+#the annoying thing is that OrderedModel.predict() returns a different datatype than OLS.predict()
+#so we have to reformat things to make it work
+y_pred = pd.DataFrame(y_pred)
+y_pred.columns = ['p1', 'p2', 'p3']
+y_pred['predicted_relevance'] = y_pred.apply(features.getPredictedRelevance, axis=1)
+y_pred.reset_index(drop=True, inplace=True)
+y_test.reset_index(drop=True, inplace=True)
+
+#i reset the indices for both tables
+#so now both tables indices go from 0 to size(test_data)-1
+#then joined them by index, which is now because of the index reset a row-wise join
+#if logit_results.predict() does not reorder the rows
+#and if reset_index does not reorder the rows 
+#this should be good
+#you can see in the console that p3 is the highest for every tuple
+#because our model is one sided because it has only one feature
+#so our model predicts every tuple to be of relevance level 3
+print(pd.concat([y_test, y_pred], axis=1)[['relevance','p1', 'p2', 'p3', 'predicted_relevance']])
+
+#you can read the confusion matrix as follows
+#on the bottom you read predicted class 0 to 2 (which is relevance level 1 to 3)
+#and for each predicted class you look up vertically
+#we have predicted all tuples to be in class 3
+#you look in the class 3 column to see how many of your class 3 predictions
+#were in reality in class1, class2, class3
+#we can see that if we predict class 3 then not alot are actually in class 1
+#but quite alot of them are actually in class 2
+#but the good thing is that most of our class 3 predictions were actually correct
+cm = confusion_matrix(y_test, y_pred['predicted_relevance'])
+disp = ConfusionMatrixDisplay(confusion_matrix= cm)
+disp.plot()
 plt.show()
